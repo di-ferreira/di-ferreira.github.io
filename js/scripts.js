d355f5be1ae90ec9899776da630d500bbc6c5815
Diego Ferreira <diferreira.program@gmail.com>
2020-06-05 12:49:20 -0300

    add validate form

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@
 //    behavior: 'smooth'
 // });
 
+/* scrollTo */
 const menuLinks = document.querySelectorAll('.navigation_link[href^="#"]');
 
 menuLinks.forEach((item) => {
@@ -64,3 +65,32 @@ function smoothScrollTo(endX, endY, duration) {
     window.scroll(newX, newY);
   }, 1000 / 60); // 60 fps
 }
+
+/* validate form */
+
+let form = $(".contact_talk_form");
+console.log(form);
+
+form.validate({
+  errorElement: "span", //por padrão, o error é 'label' mas span é mais visível
+  rules: {
+    //regras de validação:
+    name: "required", //required = é necessário!
+    email: {
+      required: true, //objeto sempre passa pelo menos dois valores.
+      email: true, // quando tem mais de um requisito, cria-se um objeto
+    },
+    phone: "required",
+    text: "required",
+  },
+  messages: {
+    //mensagens que estarão no span
+    name: "Por favor, informe seu nome",
+    email: {
+      required: "Por favor, digite seu e-mail",
+      email: "Por favor, digite um e-mail válido",
+    },
+    phone: "Por favor, digite seu número de contato",
+    text: "Por favor, escreva sua mensagem",
+  },
+});

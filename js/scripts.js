daefddc2adcdec698644d5e2563f6391da148751
Diego Ferreira <diferreira.program@gmail.com>
2020-06-05 14:12:21 -0300

    Configuração netlify

--- js/scripts.js
+++ js/scripts.js
@@ -69,28 +69,39 @@ function smoothScrollTo(endX, endY, duration) {
 /* validate form */
 
 let form = $(".contact_talk_form");
-console.log(form);
+//console.log(form)
 
-form.validate({
-  errorElement: "span", //por padrão, o error é 'label' mas span é mais visível
-  rules: {
-    //regras de validação:
-    name: "required", //required = é necessário!
-    email: {
-      required: true, //objeto sempre passa pelo menos dois valores.
-      email: true, // quando tem mais de um requisito, cria-se um objeto
+let buttonSend = $(".contact_talk_form_send_button_style");
+let inputs = $(".contact_talk_form_group_control");
+
+buttonSend.click(
+  form.validate({
+    errorElement: "span", //por padrão, o error é 'label' mas span é mais visível
+    rules: {
+      //regras de validação:
+      name: "required", //required = é necessário!
+      email: {
+        required: true, //objeto sempre passa pelo menos dois valores.
+        email: true, // quando tem mais de um requisito, cria-se um objeto
+      },
+      phone: "required",
+      text: "required",
     },
-    phone: "required",
-    text: "required",
-  },
-  messages: {
-    //mensagens que estarão no span
-    name: "Por favor, informe seu nome",
-    email: {
-      required: "Por favor, digite seu e-mail",
-      email: "Por favor, digite um e-mail válido",
+    messages: {
+      //mensagens que estarão no span
+      name: "Por favor, informe seu nome",
+      email: {
+        required: "Por favor, digite seu e-mail",
+        email: "Por favor, digite um e-mail válido",
+      },
+      phone: "Por favor, digite seu número de contato",
+      text: "Por favor, escreva sua mensagem",
     },
-    phone: "Por favor, digite seu número de contato",
-    text: "Por favor, escreva sua mensagem",
-  },
-});
+    submitHandler: () => {
+      alert("Contato enviado com sucesso!!");
+      inputs.forEach((input) => {
+        input.value = "";
+      });
+    },
+  })
+);
